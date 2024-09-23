b8a9e7cb8c83c18b9c93dbe553af8b8531096b30
Barış Gündüz <baris@gunduzmedya.com>
2024-09-23 21:46:12 +0300

    Static build. unfortunately

---
+++ build/precache-manifest.b683ab48acb779681711f21f1f6973c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "830dbdcf6d9cea34ca9c",
+    "url": "/static/css/main.66508115.chunk.css"
+  },
+  {
+    "revision": "830dbdcf6d9cea34ca9c",
+    "url": "/static/js/main.1fc1b6e1.chunk.js"
+  },
+  {
+    "revision": "2dd2b283b8b88dbcd2e8",
+    "url": "/static/js/runtime~main.296d1d66.js"
+  },
+  {
+    "revision": "2e0cc0a4a422eca82bf1",
+    "url": "/static/css/2.135b2931.chunk.css"
+  },
+  {
+    "revision": "2e0cc0a4a422eca82bf1",
+    "url": "/static/js/2.2a9ca486.chunk.js"
+  },
+  {
+    "revision": "2f6110f28478fa8870ab",
+    "url": "/static/js/3.0c0dc788.chunk.js"
+  },
+  {
+    "revision": "da6ad5dd5f85f7c16c2a3c079a52fd16",
+    "url": "/index.html"
+  }
+];
